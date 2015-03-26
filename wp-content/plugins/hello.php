11d5de17bb27a984ddcf770a4845bbe1fbc97e33
roman <sitdikov.91@bk>
2015-03-26 15:45:48 +0200

    test

--- wp-content/plugins/hello.php
+++ wp-content/plugins/hello.php
@@ -12,7 +12,7 @@ Version: 1.6
 Author URI: http://ma.tt/
 */
 
-function hello_dolly_get_lyric() {
+function hello_dolly_get_lyric() {    
 	/** These are the lyrics to Hello Dolly */
 	$lyrics = "Hello, Dolly
 Well, hello, Dolly
